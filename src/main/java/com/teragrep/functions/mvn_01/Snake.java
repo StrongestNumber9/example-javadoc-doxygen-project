535839c13be823398460d25a1adefc704c430ad1
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2025-08-20 13:50:16 +0300

    Add Snake.java

---
+++ src/main/java/com/teragrep/functions/mvn_01/Snake.java
@@ -0,0 +1,28 @@
+package com.teragrep.functions.mvn_01;
+
+/**
+ * @class Snake
+ * @brief Hisses
+ *
+ * @responsibilities
+ * - Very bitey
+ *
+ * @startuml
+ * class Snake {
+ *   + hiss()
+ * }
+ *
+ * note right of Snake
+ * Responsibilities:
+ * - Very bitey
+ *
+ * end note
+ *
+ * @enduml
+ */
+
+public final class Snake {
+    public void hiss() {
+        return;
+    }
+}
